b3a34f2818d669f60fa891cbc91b00d9b58c5626
bobuskij <bobuskij@gmail.com>
2015-03-06 23:19:07 +0200

    create articles models

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150306204442) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "text",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
 end
