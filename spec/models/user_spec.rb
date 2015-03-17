e5997fab34b0ca7b100f89e4a2e2822aa31566cc
PirogovAleksey <pirogovalekseyyy@gmail.com>
2015-03-17 23:15:02 +0200

    Тест

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe User do
+  before { @user = User.new(email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  it "should be invalid" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+    addresses.each do |invalid_address|
+      @user.email = invalid_address
+      expect(@user).not_to be_valid
+    end
+  end
+
+describe "when email format is valid" do
+  it "should be valid" do
+    addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+    addresses.each do |valid_address|
+      @user.email = valid_address
+      expect(@user).to be_valid
+    end
+  end
+end
+end
