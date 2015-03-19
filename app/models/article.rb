868222090a20aaeeae0e096c318713fd3220bba2
Macrovarius <macrovarius@gmail.com>
2015-03-19 19:41:31 +0200

    Article test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-  validates_presence_of :title, :body
-  validates :score, numericality: true, allow_blank: false
+  validates_presence_of :title, :text
   validates :title, :presence => true, :length => {:minimum => 15}
+  validates :title, :presence => true, allow_blank: false
+  validates :text, :presence => true, allow_blank: false
 end
--- spec/models/article_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Article, type: :model do
-  	post = Article.new
-	assert_not post.valid?
-	assert_equal [:title, :body], post.errors.keys
-end
