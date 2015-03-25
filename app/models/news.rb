64112553d02ec1db09333c1df60d216e928b75f4
Hellbrain <123w12345@ukr.net>
2015-03-25 19:28:39 +0200

    friendship_user

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,8 @@
 class News < ActiveRecord::Base
   validates :body,:title,presence: true
   validates :title,length: {minimum: 15}
+
+  belongs_to :author, :class_name => 'User'
+  validates :author_id, presence: true
+end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,4 +43,16 @@ describe User do
       end
     end
   end
+
+	it "should have many news" do
+	news = create :news
+	user = create :user
+	end
+
+	expect(user.news.count).to eq (0)
+	user.news = [news]
+	user.save
+
+	expect(user.news.count).to eq (1)
+	end
 end
