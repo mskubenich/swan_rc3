eb15c98cc5e25fcdff1d24a5d1e78e2f5751749b
Hellbrain <123w12345@ukr.net>
2015-03-25 20:34:49 +0200

    user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+	  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   has_secure_password
+  
+   has_attached_file :avatar, :styles => {:large => "500x500>", :thumb => "100x100>"}, :default_url => '../../public/assets/default_avatar.png'
+	validates_attachment :avatar,
+	:content_type => { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] } ,
+	:size => {:in => 0..1.megabytes}
 end
