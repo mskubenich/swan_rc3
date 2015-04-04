fddf3a9e05b1a34ca290c7e35d444a2e72a01fa5
Yura Yatsko <urasuper1994@gmail.com>
2015-04-04 10:55:17 +0300

    Add CRUD for articles on Admin and changed format index.html.erb to index.html.haml

---
+++ spec/controllers/admin/article_controller_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+describe Admin::ArticlesController do
+  let(:article) do
+    create(:article, title: 'Example article title', text: 'Example title text')
+  end
+  describe "GET index" do
+    it "assigns all articles as @articles" do
+      get :index
+      puts assigns(:articles).inspect
+      assigns(:articles).should_not be_empty
+      assigns(:articles).first.should be_a_kind_of(Article)
+    end
+  end
+
+  describe 'GET #new' do
+    it "assigns a new Article to @article" do
+      get :new
+      expect(assigns(:article)).to be_a_new(Article)
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    it "assigns the requested article to @article" do
+      get :edit, id: article
+      expect(assigns(:article)).to eq article
+    end
+
+    it "renders the :edit template" do
+      get :edit, id: article
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'DELETE destroy' do
+    it "deletes the article" do
+      article
+      expect{
+        delete :destroy, id: article
+      }.to change(Article, :count).by(-1)
+    end
+
+    it "redirects to articles#index" do
+      delete :destroy, id: article
+      expect(response).to redirect_to admin_articles_path
+    end
+  end
+
+  describe 'GET #show' do
+    it "assigns the requested article to @article" do
+      get :show, id: article
+      expect(assigns(:article)).to eq article
+    end
+
+    it "renders the :show template" do
+      get :show, id: article
+      expect(response).to render_template :show
+    end
+  end
+end
