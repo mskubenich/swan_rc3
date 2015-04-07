efd702757efc15650eb79f4e62b01fe44b39c989
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-04-07 17:14:55 +0300

    Fixes.

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,7 +1,4 @@
 class News < ActiveRecord::Base
   validates_presence_of :body, :title
   validates :title, length: { minimum: 15 }
-
-  belongs_to :author, :class_name => 'User'
-  validates :author_id, presence: true
 end
