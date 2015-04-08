aa4b7e7bcc8d88e9bde2f3f8235187905c13ec61
Maksim-Koshelya <maksim.koshelya@gmail.com>
2015-04-09 01:27:42 +0300

    edit_profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render 'edit'
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'pages/index'
 
+  get 'users/edit'
+
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
